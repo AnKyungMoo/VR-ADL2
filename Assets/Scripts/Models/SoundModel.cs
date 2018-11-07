0b9f81b7d1e2176b08ea8304e9013d01d8987182
KM <sodp2@naver.com>
2018-11-07 13:21:16 +0900

    feat: add the sound model

---
+++ Assets/Scripts/Models/SoundModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundModel : MonoBehaviour {
+    private AudioClip _audioClip;
+    private float _spatialBlend;
+
+    public SoundModel()
+    {
+        _audioClip = null;
+        _spatialBlend = 1.0f;
+    }
+
+    public SoundModel(AudioClip audioClip, float spatialBlend)
+    {
+        _audioClip = audioClip;
+        _spatialBlend = spatialBlend;
+    }
+}

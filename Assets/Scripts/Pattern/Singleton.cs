adc016964dead5243eb8a5b726c46dc0cc0646ad
KM <sodp2@naver.com>
2018-11-07 12:20:36 +0900

    feat: add the singleton script

---
+++ Assets/Scripts/Pattern/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Pattern
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance = null;
+
+        public static T GetInstance { get { return _instance ?? GetInstanceObject(); } }
+
+        protected virtual void Awake()
+        {
+            _instance = this as T;
+        }
+
+        private static T GetInstanceObject()
+        {
+            _instance = FindObjectOfType<T>().GetComponent<T>();
+            return _instance;
+        }
+
+    }
+}

13d223caa97b969a60c66cac1d55f31ec50b416c
KM <sodp2@naver.com>
2018-11-08 21:59:57 +0900

    feat: add the namespace in scripts

--- Assets/Scripts/Managers/LoadManager.cs
+++ Assets/Scripts/Managers/LoadManager.cs
@@ -3,83 +3,87 @@ using System.Collections.Generic;
 using UnityEngine;
 using Pattern;
 
-public class LoadManager : Singleton<LoadManager> {
-
-    private List<SoundModel> _soundList { get; set; }
-
-    public const string DirectoryMusic = @"/Resources/Musics/";
-    public const string ExtensionMusic = @".mp3";
-
-    private struct SoundExtender
+namespace Managers
+{
+    public class LoadManager : Singleton<LoadManager>
     {
-        public SoundExtender(string name, float spatialBlend)
+
+        private List<SoundModel> _soundList { get; set; }
+
+        public const string DirectoryMusic = @"/Resources/Musics/";
+        public const string ExtensionMusic = @".mp3";
+
+        private struct SoundExtender
         {
-            Name = name;
-            SpatialBlend = spatialBlend;
+            public SoundExtender(string name, float spatialBlend)
+            {
+                Name = name;
+                SpatialBlend = spatialBlend;
+            }
+
+            public readonly string Name;
+            public readonly float SpatialBlend;
         }
 
-        public readonly string Name;
-        public readonly float SpatialBlend;
-    }
+        protected override void Awake()
+        {
+            base.Awake();
 
-    protected override void Awake()
-    {
-        base.Awake();
+            Initialize();
+        }
 
-        Initialize();
-    }
+        public void Initialize()
+        {
+            _soundList = _soundList ?? new List<SoundModel>();
 
-    public void Initialize()
-    {
-        _soundList = _soundList ?? new List<SoundModel>();
+            StartCoroutine(LoadSoundCoroutine());
+        }
 
-        StartCoroutine(LoadSoundCoroutine());
-    }
-
-    private IEnumerator LoadSoundCoroutine()
-    {
-        // 사용하는 백그라운드 뮤직들에 대한 리스트
-        var soundExtenders = new List<SoundExtender>
+        private IEnumerator LoadSoundCoroutine()
+        {
+            // 사용하는 백그라운드 뮤직들에 대한 리스트
+            var soundExtenders = new List<SoundExtender>
             {
                 new SoundExtender("washing_complete", 1f),
                 new SoundExtender("ring", 1f),
                 new SoundExtender("IU", 1f)
             };
 
-        // 각각의 뮤직 파일들을 차례차례 리스트에 로드
-        foreach (var soundExtender in soundExtenders)
-        {
-            // 정적 파일 할당
-            var staticMusicFile =
-                new WWW(@"file://" + Application.dataPath +
-                        DirectoryMusic + soundExtender.Name + ExtensionMusic);
-
-            // 정적 파일 할당을 기다림
-            while (!staticMusicFile.isDone)
+            // 각각의 뮤직 파일들을 차례차례 리스트에 로드
+            foreach (var soundExtender in soundExtenders)
             {
-                yield return new WaitForSeconds(1.0f);
+                // 정적 파일 할당
+                var staticMusicFile =
+                    new WWW(@"file://" + Application.dataPath +
+                            DirectoryMusic + soundExtender.Name + ExtensionMusic);
+
+                // 정적 파일 할당을 기다림
+                while (!staticMusicFile.isDone)
+                {
+                    yield return new WaitForSeconds(1.0f);
+                }
+
+                // 정적으로 할당된 파일을 동적으로 로드
+                var audioClip = (AudioClip)Resources.Load("Sounds/Musics/" + soundExtender.Name, typeof(AudioClip));
+
+                // 동적으로 로드한 파일의 값이 없는 경우
+                if (audioClip == null)
+                {
+                    _soundList.Add(null);
+                }
+                // 동적으로 성공적으로 로드에 성공한 경우
+                else
+                {
+                    _soundList.Add(new SoundModel(audioClip, soundExtender.SpatialBlend));
+                }
             }
 
-            // 정적으로 할당된 파일을 동적으로 로드
-            var audioClip = (AudioClip)Resources.Load("Sounds/Musics/" + soundExtender.Name, typeof(AudioClip));
-
-            // 동적으로 로드한 파일의 값이 없는 경우
-            if (audioClip == null)
-            {
-                _soundList.Add(null);
-            }
-            // 동적으로 성공적으로 로드에 성공한 경우
-            else
-            {
-                _soundList.Add(new SoundModel(audioClip, soundExtender.SpatialBlend));
-            }
+            yield return null;
         }
-        
-        yield return null;
-    }
 
-    public SoundModel GetSoundModel(int index)
-    {
-        return _soundList[index];
+        public SoundModel GetSoundModel(int index)
+        {
+            return _soundList[index];
+        }
     }
 }
--- Assets/Scripts/Models/SoundModel.cs
+++ Assets/Scripts/Models/SoundModel.cs
@@ -2,19 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundModel : MonoBehaviour {
-    private AudioClip _audioClip;
-    private float _spatialBlend;
-
-    public SoundModel()
+namespace Models
+{
+    public class SoundModel : MonoBehaviour
     {
-        _audioClip = null;
-        _spatialBlend = 1.0f;
-    }
+        private AudioClip _audioClip;
+        private float _spatialBlend;
 
-    public SoundModel(AudioClip audioClip, float spatialBlend)
-    {
-        _audioClip = audioClip;
-        _spatialBlend = spatialBlend;
+        public SoundModel()
+        {
+            _audioClip = null;
+            _spatialBlend = 1.0f;
+        }
+
+        public SoundModel(AudioClip audioClip, float spatialBlend)
+        {
+            _audioClip = audioClip;
+            _spatialBlend = spatialBlend;
+        }
     }
 }
